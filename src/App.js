f4e377f3a579e21fbf921e09bbab6cdc12bd9ab4
KAlexej <90187807+KAlexej@users.noreply.github.com>
2021-09-09 18:35:42 +0300

    homework-2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import Layout from "./components/Layout";
+import Footer from "./components/Footer";
+
+import LayoutBG from './assets/bg1.jpg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World, React.JS!
-        </p>
-      </header>
-    </div>
-  );
+    const Title = 'This is title';
+    const Desc = 'This is Description!';
+    const colorBg = 'red';
+    return (
+        <>
+            <Header title={Title} desc={Desc} />
+            <Layout title={Title} desc={Desc} urlBg={LayoutBG}/>
+            <Layout title={Title} desc={Desc} colorBg={colorBg}/>
+            <Layout title={Title} desc={Desc} urlBg={LayoutBG}/>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Footer/Header.js
@@ -0,0 +1,17 @@
+import style from './style.module.css';
+
+const Header = () => {
+    return (
+        <>
+            <header className="root">
+                <div className="forest"></div>
+                <div className="container">
+                    <h1>This is title</h1>
+                    <p>This is Description!</p>
+                </div>
+            </header>
+        </>
+    );
+}
+
+export default Header;
